184a62f1b4c886b8b8e6b7112f992985662e9930
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-08-03 12:58:10 +0300

    add MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,21 @@
-const MovieList = () => {
-  return <>MovieList</>;
+import { useLocation } from 'react-router-dom';
+import { List, ListItem, StyledLink } from './MovieList.styled';
+
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+  return (
+    <>
+      <List>
+        {movies?.map(({ id, title, name }) => (
+          <ListItem key={id}>
+            <StyledLink to={`/movies/${id}`} state={{ from: location }}>
+              {title ?? name}
+            </StyledLink>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
 };
 
 export default MovieList;
--- src/components/MovieList/MovieList.styled.js
+++ src/components/MovieList/MovieList.styled.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
 
-export const ListItem = styled.li``;
+export const ListItem = styled.li`
+  color: #4f4141;
+`;
 
-export const StyledLink = styled.li``;
+export const StyledLink = styled(Link)`
+  font-size: 16px;
+  color: #4f4141;
+`;
