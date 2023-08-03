f6b15886efe58c0a3e5b413aa11c00bd1c805352
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-08-03 13:31:28 +0300

    add Movies

---
+++ src/pages/Movies/Movies.styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 30px 20px 20px 30px;
+`;
+
+export const SearchForm = styled.form``;
+
+export const SearchInput = styled.input`
+  font-size: 18px;
+  padding: 8px 12px;
+  width: 250px;
+
+  border: 2px solid #000000;
+  border-radius: 6px;
+`;
+
+export const SearchBtn = styled.button`
+  font-size: 16px;
+  padding: 10px 12px;
+
+  border: 2px solid #000000;
+  border-radius: 6px;
+  cursor: pointer;
+`;
+
+export const Loader = styled.div`
+  display: flex;
+  justify-content: center;
+`;
